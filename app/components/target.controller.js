5835967e7a581466fdd888f8470265f8792a5709
Brett Wilcox <wilcox.brett@gmail.com>
2019-02-14 19:59:16 -0700

    Button logic so no over/under 0/100.

--- app/components/target.controller.js
+++ app/components/target.controller.js
@@ -7,9 +7,62 @@ function draw() {
   document.getElementById('health').innerText = update.health
   document.getElementById('hits').innerText = update.hits
   document.getElementById('progress').style.width = update.progress + '%';
+
+  //button logic
+  document.getElementById('firework').disabled = false
+  document.getElementById('ninjaStar').disabled = false
+  if (update.health <= 90) {
+    document.getElementById('noodles').disabled = false
+  }
+  if (update.health > 90) {
+    document.getElementById('noodles').disabled = true
+  }
+  if (update.health < 10) {
+    document.getElementById('kick10').disabled = true
+  } else {
+    document.getElementById('kick10').disabled = false
+  }
+  if (update.health < 5) {
+    document.getElementById('punch5').disabled = true
+  } else {
+    document.getElementById('punch5').disabled = false
+  }
+  if (update.health < 1) {
+    document.getElementById('slap1').disabled = true
+  } else {
+    document.getElementById('slap1').disabled = false
+  }
+  if (update.health == 0) {
+    document.getElementById('noodles').disabled = true
+  }
+  if (update.health < 6) {
+    document.getElementById('ninjaStar').disabled = true
+  }
+  if (update.health < 4) {
+    document.getElementById('firework').disabled = true
+  }
+
+  //change panda picture depending on health
+  if (update.health == 100) {
+    document.getElementById('pandaTarget').setAttribute("src", "assets/img/panda-start.png")
+  }
+  if (update.health < 100) {
+    document.getElementById('pandaTarget').setAttribute("src", "assets/img/panda-healthy.png")
+  }
+  if (update.health < 70) {
+    document.getElementById('pandaTarget').setAttribute("src", "assets/img/panda-block1.png")
+  }
+  if (update.health < 30) {
+    document.getElementById('pandaTarget').setAttribute("src", "assets/img/panda-block2.png")
+    document.getElementById('pandaTarget').setAttribute("style", "width: 240px")
+  }
+  if (update.health < 1) {
+    document.getElementById('pandaTarget').setAttribute("src", "assets/img/panda-defeated.png")
+    document.getElementById('pandaTarget').setAttribute("style", "margin-top: 2vh")
+  }
 }
 
-//Public
+//Public///////////////////////////////////////////////
 export default class TargetController {
   constructor() {
     _ts.addSubscriber('target', draw)
--- app/components/target.service.js
+++ app/components/target.service.js
@@ -28,10 +28,10 @@ function setState(dataName, val) {
   _subscribers[dataName].forEach(fn => fn())
 }
 
-let itemModifier = []
+let _itemModifier = []
 
 
-//Public
+//Public/////////////////////////////////////////
 export default class TargetService {
   constructor() {}
 
@@ -44,10 +44,13 @@ export default class TargetService {
   }
 
   attack(type) {
+    //add modifier items if any
     let modDamage = 0
-    if (itemModifier.length != 0) {
-      modDamage = itemModifier.shift()
+    if (_itemModifier.length != 0) {
+      modDamage = _itemModifier.shift()
     }
+
+    //attack buttons enabled/disabled and attack
     let damage = 0
     if (_state.target.attacks[type]) {
       damage = _state.target.attacks[type]
@@ -55,27 +58,50 @@ export default class TargetService {
         ..._state.target
       }
       updatedTarget.health -= damage + modDamage
+      updatedTarget.progress -= damage + modDamage
       updatedTarget.hits++
       setState('target', updatedTarget)
     }
   }
 
   item(type) {
-    let change
-    if (change = _state.target.items[type]) {
-      itemModifier.push(change)
+    let change = 0
+    if (_state.target.items[type]) {
+      change = _state.target.items[type]
+      _itemModifier.push(change)
+      //disable item buttons when one is selected
+      document.getElementById('firework').disabled = true
+      document.getElementById('ninjaStar').disabled = true
+    }
+    //disable attack buttons depending on which modifier item selected
+    if (type == 'firework') {
+      if (_state.target.health < 13) {
+        document.getElementById('kick10').disabled = true;
+      }
+      if (_state.target.health < 8) {
+        document.getElementById('punch5').disabled = true;
+      }
+    }
+    if (type == 'ninjaStar') {
+      if (_state.target.health < 15) {
+        document.getElementById('kick10').disabled = true;
+      }
+      if (_state.target.health < 10) {
+        document.getElementById('punch5').disabled = true;
+      }
     }
   }
 
   eat(type) {
-    let feedNoodles
+    let feedNoodles = 0
     if (_state.target.items[type]) {
       feedNoodles = _state.target.items[type]
       let updatedTarget = {
         ..._state.target
       }
       updatedTarget.health += feedNoodles
-      setState('target', feedNoodles)
+      updatedTarget.progress += feedNoodles
+      setState('target', updatedTarget)
     }
   }
 
@@ -99,5 +125,4 @@ export default class TargetService {
     setState('target', newTarget)
     $('#gameOver').modal('hide')
   }
-
 }
